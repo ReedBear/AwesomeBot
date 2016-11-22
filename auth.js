14a15de2bef55e6267f0e07903908d68763aab61
Julien Seren-Rosso <julien@coorpacademy.com>
2016-11-22 18:19:10 +0100

    fix

--- auth.js
+++ auth.js
@@ -3,13 +3,5 @@ module.exports = {
     "client_id": process.env.DISCORD_CLIENT_ID,
     "server_port": 8080,
     "server_ip_address": "127.0.0.1",
-    "avatar_url": "http://vignette1.wikia.nocookie.net/playstationallstarsfanfictionroyale/images/9/9f/Claptrap.png/revision/latest?cb=20130609075927",
-    "google_api_key": "",
-    "custom_search_id": "",
-    "giphy_api_key": "",
-    "imgur_client_id": "",
-    "wolfram_app_id": "",
-    "openexchangerates_app_id": "",
-    "microsoft_client_id": "",
-    "microsoft_client_secret": ""
+    "avatar_url": "http://vignette1.wikia.nocookie.net/playstationallstarsfanfictionroyale/images/9/9f/Claptrap.png/revision/latest?cb=20130609075927"
 }
