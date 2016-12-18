86e24b8a81018c6264c52c05ddcd97ed0bbe7764
Arthur Jamain <arthur.jamain@gmail.com>
2016-12-18 16:32:55 +0100

    ...

--- auth.js
+++ auth.js
@@ -2,14 +2,9 @@ module.exports = {
     "token": process.env.DISCORD_TOKEN,
     "client_id": process.env.DISCORD_CLIENT_ID,
     "server_port": process.env.PORT,
-    "server_ip_address": "127.0.0.1",
     "giphy_api_key": process.env.GIPHY,
     "avatar_url": "http://vignette1.wikia.nocookie.net/playstationallstarsfanfictionroyale/images/9/9f/Claptrap.png/revision/latest?cb=20130609075927",
-    "google_api_key": "",
     "custom_search_id": process.env.GOOGLE_CUSTOM_SEARCH,
     "imgur_client_id": process.env.IMGUR_CLIENT_ID,
-    "wolfram_app_id": process.env.WOLFRAM_API,
-    "openexchangerates_app_id": "",
-    "microsoft_client_id": "",
-    "microsoft_client_secret": ""
+    "wolfram_app_id": process.env.WOLFRAM_API
 }
