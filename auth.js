a9ae0c27eb0502db13278020711618879741dcdc
Arthur Jamain <arthur.jamain@gmail.com>
2016-12-18 15:56:47 +0100

    port var

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 module.exports = {
     "token": process.env.DISCORD_TOKEN,
     "client_id": process.env.DISCORD_CLIENT_ID,
-    "server_port": 8080,
+    "server_port": process.env.PORT,
     "server_ip_address": "127.0.0.1",
     "avatar_url": "http://vignette1.wikia.nocookie.net/playstationallstarsfanfictionroyale/images/9/9f/Claptrap.png/revision/latest?cb=20130609075927"
 }
